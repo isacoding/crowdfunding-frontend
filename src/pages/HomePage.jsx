d2b4218a92ad5f2085a846bf9bb162f8998b1ba6
Isabela <isabela.iar01@gmail.com>
2024-11-26 19:05:41 +1000

    .env added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
-import { allProjects } from "../data";
+// import { allProjects } from "../data";
+import useProjects from "../hooks/use-projects";
 import ProjectCard from "../components/ProjectCard";
 import "./HomePage.css";
 
 function HomePage() {
+  const { projects } = useProjects();
   // return <h1>This is the home page.</h1>;
   return (
     // <div>
     <div id="project-list">
-      {allProjects.map((projectData, key) => {
+      {projects.map((projectData, key) => {
         // return <div key={key}>{projectData.title}</div>;
         return <ProjectCard key={key} projectData={projectData} />;
       })}
